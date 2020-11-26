3f6e2c2b72470e43941b5cba0e4f7521dc0de82b
Marty Warfel <marthawarfel@gmail.com>
2020-11-26 03:05:30 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+programmer_hash[:grace_hopper]
 
 end
 
@@ -36,8 +37,15 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+ 
+ programmer_hash[:alan_kay][:known_for]
+  
+end 
+  
 
-end
+
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +63,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -81,6 +90,14 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[0] =
+  {
+  :yukihiro_matsumoto => { 
+    :known_for => "Ruby",
+    :languages =>  ["LISP", "C"] 
+  }
+}
+  
 
 end
 
@@ -104,6 +121,9 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +145,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 
 end
